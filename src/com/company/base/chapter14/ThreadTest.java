e0f9566cf4c83f34f3942cac158231c1faa249d5
ccccye <w0.0.3@163.com>
2020-06-15 17:38:41 +0800

    线程池-ThreadPoolExecutor构造方法实现

--- src/com/company/base/chapter14/ThreadTest.java
+++ src/com/company/base/chapter14/ThreadTest.java
@@ -48,6 +48,14 @@ public class ThreadTest {
         });
 
 
+        ExecutorService threadPoolExecutor = new ThreadPoolExecutor(2 // 最少线程资源数量
+                , 16 // 最多线程资源数量
+                , 1000 // 线程空闲自动销毁时间,具体时间还要看时间单位
+                , TimeUnit.SECONDS  // 时间单位  秒
+                , new LinkedBlockingQueue()  // 任务队列类型   链表
+                ,Executors.defaultThreadFactory() // 线程工厂,一般使用默认的
+                , new ThreadPoolExecutor.AbortPolicy());  // 拒绝任务时的处理策略  丢弃任务报异常
+
 //
 //        System.out.println("主线程:"+Thread.currentThread().getId());
 //
