16c06d4c9ea38269f2ce6ca97e874ea005de21de
ccccye <w0.0.3@163.com>
2020-06-15 11:49:38 +0800

    线程池-Executors实现

--- src/com/company/base/chapter14/ThreadTest.java
+++ src/com/company/base/chapter14/ThreadTest.java
@@ -1,22 +1,68 @@
 package com.company.base.chapter14;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.FutureTask;
-import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.*;
 
 public class ThreadTest {
     public static void main(String[] args) throws InterruptedException, ExecutionException {
-        System.out.println("主线程:"+Thread.currentThread().getId());
+        // 创建线程池的7种方法
+        // 1 创建可缓存的线程池   无可用线程自动创建，线程池可不限大
+       ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
+       cachedThreadPool.execute(new Runnable() {
+           @Override
+           public void run() {
+               System.out.println("子线程："+Thread.currentThread().getId());
+           }
+       });
 
-        CallableThread call = new CallableThread();
-        FutureTask<Integer> ft = new FutureTask<>(call);
+       // 2 创建定长线程池，超出的线程在队列等待
+        ExecutorService fixedThreadPool =  Executors.newFixedThreadPool(3);
+        fixedThreadPool.execute(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("fixed thread 子线程："+Thread.currentThread().getId());
+            }
+        });
 
-        System.out.println("a");
-        Thread th = new Thread(ft);
-        th.start();
-        System.out.println("b");
-        System.out.println(ft.get());  // 如果线程没有返回将阻塞
+        // 3 创建定长，可周期性任务执行
+        ScheduledExecutorService scheduledExecutorService= Executors.newScheduledThreadPool(2);
+        scheduledExecutorService.schedule(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("delay 3 seconds");
+            }
+        }, 3, TimeUnit.SECONDS);
+        scheduledExecutorService.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("周期性线程池"+Thread.currentThread().getId());
+            }
+        }, 1, 3, TimeUnit.SECONDS);
+
+        // 4 创建单线程的线程池， 只有1个线程，任务队列可以指定  FIFO  LIFO
+        ExecutorService singleThreadService = Executors.newSingleThreadExecutor();
+        singleThreadService.execute(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("single thread 子线程："+Thread.currentThread().getId());
+            }
+        });
+
+
+//
+//        System.out.println("主线程:"+Thread.currentThread().getId());
+//
+//        CallableThread call = new CallableThread();
+//        FutureTask<Integer> ft = new FutureTask<>(call);
+//
+//        System.out.println("a");
+//        Thread th = new Thread(ft);
+//        th.start();
+//        System.out.println("b");
+//        System.out.println(ft.get());  // 如果线程没有返回将阻塞
+//
+//        Thread.sleep(100);
+//        th.wait();
+//
 
         //        Runnable runnable = new MyRunnableThread();
 //        Thread th = new Thread(runnable);
