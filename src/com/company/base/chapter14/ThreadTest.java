7b1aec673de9e33962805d549e2ea34c91eb03b6
ccccye <w0.0.3@163.com>
2020-06-16 08:54:19 +0800

    手动锁lock

--- src/com/company/base/chapter14/ThreadTest.java
+++ src/com/company/base/chapter14/ThreadTest.java
@@ -1,18 +1,41 @@
 package com.company.base.chapter14;
 
 import java.util.concurrent.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class ThreadTest {
     public static void main(String[] args) throws InterruptedException, ExecutionException {
+        // 创建手动锁
+        Lock lock = new ReentrantLock();
+        lock.lock();
+        //lock.tryLock(5, TimeUnit.SECONDS;
+        try{
+            System.out.println("获得了锁");
+        }catch(Exception e){
+
+        }finally{
+            lock.unlock();
+        }
+
+        ThreadLocal local = new ThreadLocal<String>();
+
+        local.set("sss");
+        System.out.println(local.get());
+
         // 创建线程池的7种方法
         // 1 创建可缓存的线程池   无可用线程自动创建，线程池可不限大
        ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
        cachedThreadPool.execute(new Runnable() {
            @Override
            public void run() {
+               System.out.println("子线程获取:"+local.get());
+               local.set("abcd");
+
                System.out.println("子线程："+Thread.currentThread().getId());
            }
        });
+        System.out.println(local.get());
 
        // 2 创建定长线程池，超出的线程在队列等待
         ExecutorService fixedThreadPool =  Executors.newFixedThreadPool(3);
@@ -48,13 +71,14 @@ public class ThreadTest {
         });
 
 
-        ExecutorService threadPoolExecutor = new ThreadPoolExecutor(2 // 最少线程资源数量
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(2 // 最少线程资源数量
                 , 16 // 最多线程资源数量
                 , 1000 // 线程空闲自动销毁时间,具体时间还要看时间单位
                 , TimeUnit.SECONDS  // 时间单位  秒
                 , new LinkedBlockingQueue()  // 任务队列类型   链表
                 ,Executors.defaultThreadFactory() // 线程工厂,一般使用默认的
                 , new ThreadPoolExecutor.AbortPolicy());  // 拒绝任务时的处理策略  丢弃任务报异常
+
 
 //
 //        System.out.println("主线程:"+Thread.currentThread().getId());
